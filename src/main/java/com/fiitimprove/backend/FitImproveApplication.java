de0543838278d679ad72f275b2540765dd54ce09
Sofia <sofapokl@gmail.com>
2025-03-26 11:34:30 +0200

    first commit

---
+++ src/main/java/com/fiitimprove/backend/FitImproveApplication.java
@@ -0,0 +1,13 @@
+package com.fiitimprove.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitImproveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitImproveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fiitimprove/backend/FitImproveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiitimprove.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitImproveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
